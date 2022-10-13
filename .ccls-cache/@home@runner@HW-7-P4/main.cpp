2b97edda823960fab5e46af536190845b0c11e0e
Garrett Searls <112958792+grsearls@users.noreply.github.com>
2022-10-13 04:36:53 +0000

    if statement to find the day with least amount of junk

--- .ccls-cache/@home@runner@HW-7-P4/main.cpp
+++ .ccls-cache/@home@runner@HW-7-P4/main.cpp
@@ -8,19 +8,24 @@ int main()
 
   int n;
   int junk[n];
-  int least;
+  int least = -1;
+  int days;
   cin >> n;
 
   for(int i = 0; i < 0; i++)
     {
       cin >> junk[i];
 
-      
-
-      
+      if(junk[i] < least)
+      {
+        least = junk[i];
+        days = i;
+      }
     }
 
-  
+  cout << days << endl;
+
+  return 0;
   
 }
 
