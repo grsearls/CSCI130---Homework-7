9f0e5989e01e57a427a875cbec53125e1d2eb25a
Garrett Searls <112958792+grsearls@users.noreply.github.com>
2022-10-13 02:58:35 +0000

    made function named time

--- .ccls-cache/@home@runner@HW-7-P2/main.cpp
+++ .ccls-cache/@home@runner@HW-7-P2/main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 
+using namespace std;
+
+void time(int seconds, int hours, int minutes, int secs)
+{
+  hours = seconds / 3600;
+
+  seconds -= hours * 3600;
+
+  minutes = seconds / 60;
+
+  seconds -= minutes * 60;
+
+  secs = seconds;
+}
+
 int main() {
-  std::cout << "Hello World!\n";
+
+  
+
+  
 }
